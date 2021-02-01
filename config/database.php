c06fcf5f7615e24d7161c27d39f42e8a1f4dac76
Ekojoka Christolight Idakwo <chris.idakwo@gmail.com>
2021-02-01 17:07:26 +0100

    updated
    Signed-off-by: Ekojoka Christolight Idakwo <chris.idakwo@gmail.com>

--- config/database.php
+++ config/database.php
@@ -1,13 +1,12 @@
 <?php
 
 $jawsDB = parse_url(getenv('JAWSDB_URL'));
-//$jawsDB = parse_url('mysql://p8s22rul377blco2:eaa5sa0ou70nnsum@z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jb8z9756k6v4nisu');
 
 // Database credentials
-define('DB_SERVER', $jawsDB['host']);
-define('DB_USERNAME', $jawsDB['user']);
-define('DB_PASSWORD', $jawsDB['pass']);
-define('DB_NAME', $jawsDB['path']);
+define('DB_SERVER', 'z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'p8s22rul377blco2');
+define('DB_PASSWORD', 'eaa5sa0ou70nnsum');
+define('DB_NAME', 'jb8z9756k6v4nisu');
 
 // Attempt to connect to MySQL database
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
