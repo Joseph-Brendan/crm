2c3fef5775726ac9a842900cb7de4bade2f0c1b4
Ekojoka Christolight Idakwo <chris.idakwo@gmail.com>
2021-02-01 16:26:31 +0100

    Updated database connection configs for Heroku
    Signed-off-by: Ekojoka Christolight Idakwo <chris.idakwo@gmail.com>

--- config/database.php
+++ config/database.php
@@ -1,10 +1,13 @@
 <?php
 
+$jawsDB = parse_url(getenv('JAWSDB_URL'));
+//$jawsDB = parse_url('mysql://p8s22rul377blco2:eaa5sa0ou70nnsum@z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jb8z9756k6v4nisu');
+
 // Database credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'chrisidakwo');
-define('DB_NAME', 'na-crm');
+define('DB_SERVER', $jawsDB['host']);
+define('DB_USERNAME', $jawsDB['user']);
+define('DB_PASSWORD', $jawsDB['pass']);
+define('DB_NAME', $jawsDB['path']);
 
 // Attempt to connect to MySQL database
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
